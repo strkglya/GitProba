2f3ad105ae84cdde7f9a0616b5635caaf2d9af99
strkglya <109280479+strkglya@users.noreply.github.com>
2022-10-06 14:15:49 +0300

    Update ViewController.swift

--- GitBranch/ViewController.swift
+++ GitBranch/ViewController.swift
@@ -11,9 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("This is branch #1")
         print("Added from GitHub")
     }
-    
 }
 
