3eedf7bb7e31090181c865bd51a4a8736830b0d7
Александра Среднева <starayakeglua@MacBook-Air-Aleksandra.local>
2022-10-06 12:11:26 +0300

    First commit with gitignore

---
+++ GitBranch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitBranch
+//
+//  Created by Александра Среднева on 6.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
